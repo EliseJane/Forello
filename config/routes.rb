7ee0c785c6db583f57db771eb3417baa0985d498
Elise Olivares <elisejpo@gmail.com>
2017-10-17 21:16:12 -0700

    updated email

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
 # do we need to create boards#index and boards#show routes if we want to
-# be able access those urls directly? We would then need a boards controller
-# and view files
+# be able access those urls directly? would we then need a boards controller
+# and view files?
 
   namespace :api do
     resources :boards, only: [:index, :create, :show]
