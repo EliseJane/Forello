403d43a7a5368c4e58913e94b5c9d3f1e5a7efc2
Nitin Savant <nitin@Nitins-MacBook-Air.local>
2017-10-18 22:37:10 -0500

    Added CardsReducer and ListsReducer tests

--- app/javascript/reducers/CardsReducer.js
+++ app/javascript/reducers/CardsReducer.js
@@ -1,7 +1,6 @@
 export default function listsReducer(state = [], action) {
   if (action.type === 'FETCH_BOARD_SUCCESS') {
     const { lists } = action.board;
-
     return lists.reduce((acc, list) => {
       return acc.concat(list.cards);
     }, []);
---
+++ app/javascript/reducers/ListsReducer.test.js
@@ -0,0 +1,34 @@
+import reducer from './ListsReducer';
+import * as types from '../constants/ActionTypes';
+
+describe("ListsReducer", () => {
+  describe("unknown type", () => {
+    it("returns the state parameter", () => {
+      expect(
+        reducer("param value", { type: "FAKE_TYPE_FOR_TEST" })
+      ).toEqual("param value");
+    });
+  });
+
+  describe("FETCH_BOARD_SUCCESS", () => {
+    it("returns empty array from action.board value with no lists", () => {
+      expect(
+        reducer([], {
+          type: types.FETCH_BOARD_SUCCESS,
+          board: { id: 1, title: "My board", lists: [] }
+        })
+      ).toEqual([]);
+    });
+  });
+
+  describe("FETCH_BOARD_SUCCESS", () => {
+    it("returns just lists from action.board value", () => {
+      expect(
+        reducer([], {
+          type: types.FETCH_BOARD_SUCCESS,
+          board: { id: 1, title: "My board", lists: [{ id: 1, title: "My list" }] }
+        })
+      ).toEqual([{id: 1, title: "My list"}]);
+    });
+  });
+});
