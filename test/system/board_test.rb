239510753b21d0e99650d6d13497a71c58e5f077
Sunny <sjbeatteay@gmail.com>
2017-10-20 11:26:25 -0400

    add board system test

--- test/system/board_test.rb
+++ test/system/board_test.rb
@@ -19,11 +19,21 @@ class BoardTest < ApplicationSystemTestCase
 
   test "displaying 1 card in a list" do
     create(:board, id: 1)
-    create(:list, id: 1, board_id: 1, position: 1.0)
     create(:list, id: 2, board_id: 1, position: 2.0)
     create(:card, list_id: 2)
     visit board_path(1)
 
-    assert_selector ".list-wrapper:nth-of-type(2) .card", count: 1
+    assert_selector ".list-wrapper .card", count: 1
+  end
+
+  test "displaying 2 cards in a correct list" do
+    create(:board, id: 1)
+    create(:list, id: 1, board_id: 1, position: 1.0)
+    create(:list, id: 2, board_id: 1, position: 2.0)
+    create(:card, list_id: 2)
+    create(:card, list_id: 2)
+    visit board_path(1)
+
+    assert_selector ".list-wrapper:nth-of-type(2) .card", count: 2
   end
 end
