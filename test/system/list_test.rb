ebc3eb2bb26396606ac481939f23774e38af4c56
Sunny <sjbeatteay@gmail.com>
2017-10-21 00:21:31 -0400

    fix system test

--- test/system/list_test.rb
+++ test/system/list_test.rb
@@ -2,29 +2,18 @@ require "application_system_test_case"
 
 class ListTest < ApplicationSystemTestCase
   class CreatingListTest < ApplicationSystemTestCase
-    # test "creating with a blank title does not save the board" do
-    #   visit "/"
-    #
-    #   find(".board-tile").click
-    #
-    #   within ".board-tile" do
-    #     find("input[type='text']").set("")
-    #     click_on "Create"
-    #   end
-    #
-    #   assert_equal Board.count, 0
-    #   assert_selector ".board-tile input[type='text']"
-    # end
-    #
-    # test "creating a list successfully" do
-    #   create(:board)
-    #   visit board_path(1)
-    #
-    #   find("#new-list span").click
-    #   fill_in("title", with: "My list")
-    #   find("#new-list .button").click
-    #
-    #   assert_equal 1, List.count
-    # end
+    test "creating a list successfully" do
+      create(:board, id: 1)
+      visit board_path(1)
+
+      find("#new-list span").click
+
+      within "#new-list" do
+        fill_in("title", with: "My list")
+        click_button('Save')
+      end
+
+      assert_equal List.count, 1
+    end
   end
 end
