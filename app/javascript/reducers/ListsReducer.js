5195d6138aac138555c395960ab93304aec614eb
Sunny <sjbeatteay@gmail.com>
2017-10-21 22:22:28 -0400

    add sorting to list reducer

--- app/javascript/reducers/ListsReducer.js
+++ app/javascript/reducers/ListsReducer.js
@@ -1,7 +1,7 @@
 export default function listsReducer(state = [], action) {
   if (action.type === 'FETCH_BOARD_SUCCESS') {
     const { lists } = action.board;
-    return lists;
+    return lists.sort((a, b) => a.position - b.position);
   } else if (action.type === 'CREATE_LIST_SUCCESS') {
     const newList = action.list;
     newList.position = Number(newList.position);
@@ -21,7 +21,7 @@ export default function listsReducer(state = [], action) {
       } else {
         return list;
       }
-    });
+    }).sort((a, b) => a.position - b.position);
   } else {
     return state;
   }
